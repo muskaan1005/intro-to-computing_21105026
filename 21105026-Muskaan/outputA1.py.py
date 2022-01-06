2f77ba710ccc2244783218ba3706a5aabbe41895
muskaan1005 <97123181+muskaan1005@users.noreply.github.com>
2022-01-06 21:20:55 +0530

    Add files via upload

---
+++ 21105026-Muskaan/outputA1.py.py
@@ -0,0 +1,19 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/Lenovo/AppData/Local/Programs/Python/Python310/input_A1.py =
+enter first number: 4
+enter second number: 3
+enter third number: 2
+The average is  3.0
+Gross income: 67000
+Dependents: 5
+Income tax:  8400.0
+Student=[SID, Name, Gender, DepartmentName, CGPA]
+Student= [11, 'Nikhil', 'M', 'Mechanical', 8.9]
+Student= [15, 'Mudita', 'F', 'Electronics', 9.0]
+Marks unsorted=  [11, 76, 34, 56, 30]
+Marks sorted=  [11, 30, 34, 56, 76]
+color=['Red','Green','White','Black','Pink','Yellow']
+color ['Red', 'Green', 'White', 'Pink', 'Yellow']
+color ['Red', 'Green', 'White', 'Purple', 'Yellow']
