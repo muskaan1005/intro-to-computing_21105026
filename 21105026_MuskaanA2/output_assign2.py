020c2aca50b14152ae1899ecf12a4720fa81e5fe
muskaan1005 <97123181+muskaan1005@users.noreply.github.com>
2022-01-22 15:17:22 +0530

    Add files via upload

---
+++ 21105026_MuskaanA2/output_assign2.py
@@ -0,0 +1,48 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: F:\college stuff\esc 1\assignment\21105026_MuskaanA2\input_assign2.py
+Q1
+35
+egaugnal evitisnes esac a si nohtyP
+a case sensitive
+Python is object oriented language
+index of a is: 10
+Pythonisacasesensitivelanguage
+
+Q2
+Enter your name:ABc
+Enter student ID:21105026
+Enter department name:ece
+Enter CGPA:10
+Hey, ABc Here!
+My SID is 21105026
+I am from ece department and my CGPA is 10.0
+
+Q3
+a&b= 8
+a|b= 58
+a^b= 50
+
+left shift a by 2 units= 224
+left shift b by 2 units= 40
+
+right shift a by 2 units= 14
+right shift b by 4 units= 0
+
+Q4
+enter first number=2
+enter second number=4.5
+enter third number=9
+the greatest number is 9.0
+
+Q5
+enter the string:my name is muskaan.
+Yes
+
+Q6
+enter length of first side=4
+enter length of second side=5
+enter length of third side=7
+Is the triangle possible?
+Yes
