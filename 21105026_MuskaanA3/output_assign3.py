9ef986e8e5b4e02e8cb817bba98ec9370639930f
muskaan1005 <97123181+muskaan1005@users.noreply.github.com>
2022-02-14 17:25:27 +0530

    Add files via upload

---
+++ 21105026_MuskaanA3/output_assign3.py
@@ -0,0 +1,95 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: F:\college stuff\esc 1\assignment\21105026_MuskaanA3\input_assign3.py
+Q1
+Enter a word or sentence-engineering
+e:3
+n:3
+g:2
+i:2
+r:1
+
+Q2
+Please enter Day- 10
+Please enter Month- 05
+Please enter Year- 2003
+Next Date is: 11/5/2003
+
+Q3
+Enter Y to enter data:y
+Enter a number: 2
+Enter Y to enter data:y
+Enter a number: 3.4
+Enter Y to enter data:Y
+Enter a number: 13
+Enter Y to enter data:Y
+Enter a number: 34
+Enter Y to enter data:y
+Enter a number: 56
+Enter Y to enter data:n
+[2.0, 3.4, 13.0, 34.0, 56.0]
+[(2.0, 4.0), (3.4, 11.559999999999999), (13.0, 169.0), (34.0, 1156.0), (56.0, 3136.0)]
+
+Q4
+Enter Grade Points-9
+Your Grade is A and Excellent performance 
+
+Q5
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+      
+Q6
+Enter SID:21105026
+Enter name of the student:muskaan
+Do you want to enter another entry?(Y/N)y
+Enter SID:21103040
+Enter name of the student:vanshika
+Do you want to enter another entry?(Y/N)y
+Enter SID:21105031
+Enter name of the student:gunjan
+Do you want to enter another entry?(Y/N)y
+Enter SID:21105095
+Enter name of the student:pranit
+Do you want to enter another entry?(Y/N)n
+{21105026: 'muskaan', 21103040: 'vanshika', 21105031: 'gunjan', 21105095: 'pranit'}
+
+PART A
+SID 21105026 is muskaan.
+SID 21103040 is vanshika.
+SID 21105031 is gunjan.
+SID 21105095 is pranit.
+
+PART B
+['gunjan', 'muskaan', 'pranit', 'vanshika']
+
+PART C
+[21103040, 21105026, 21105031, 21105095]
+
+PART D
+Enter SID of the student:21105026
+name:muskaan
+Q7
+Total elements of Fibonacci sequence that you want(must be greater than 1)- 3
+Fibonnaci sequence with 3 terms
+0
+1
+1
+Sum of total 3 terms of sequence is 2
+Average= 0.6666666666666666
+End
+Q8
+PART A
+set with elements in set1 and set2 but not common: {1, 3, 5, 6, 8}
+PART B
+Set of elements that are only present in not more than one set: {17, 3, 6, 8, 9, 13}
+PART C
+Set of elements that are only present in two sets: {1, 2, 4, 5}
+PART D
+set of all integers in the range 1 to 10 that are not in Set1: {6, 7, 8, 9, 10}
+PART E
+Set of all integers in the range 1 to 10 that are not in Set1 and Set2 and Set3: {10, 7}
